71a053d8aede8d71aac5a307a5b34f1c66f51165
Octo8080 <okutannderuta0529@gmail.com>
2022-06-04 22:30:26 +0900

    middlewares update

--- middleweres/oauth2.ts
+++ middleweres/oauth2.ts
@@ -1,17 +1,46 @@
-import { Context } from "../deps.ts";
+import { Context as OakContext } from "../deps.ts";
 import { accessResourceRequestCheck } from "../validates/oauth2_validates.ts";
 import { logError } from "../util/logger.ts";
 
+interface AuthSuccess {
+  status: true;
+  clientId: string;
+}
+
+interface AuthFailure {
+  status: false;
+}
+
+type Auth = AuthFailure | AuthSuccess;
+
+export type Context = OakContext & {
+  auth?: Auth;
+};
+
+export function isAuthSuccess(rawArg:unknown): rawArg is AuthSuccess{
+  const arg = rawArg as {[key:string]:unknown}
+
+  if(!arg) return false
+  if(!arg.status ) return false
+  if(typeof arg.status !== "boolean")  return false
+  if(arg.status !== true)  return false
+
+  return true
+}
+
 export async function beforeResourceAccess(
   context: Context,
-  next: () => Promise<unknown>,
+  next: () => Promise<unknown>
 ) {
   const copyRequest = Object.assign(context.request);
   const result = await accessResourceRequestCheck(copyRequest);
   if (!result.status) {
     logError("Invalid Access !!");
     context.response.status = 400;
+    context.auth = { status: false };
     return;
   }
+  context.auth = { status: true, clientId: result.clientId };
+
   await next();
 }
